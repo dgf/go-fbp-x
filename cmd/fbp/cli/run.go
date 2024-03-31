bdc806b2d804721b601e17efb073ca0067ec8962
dgf <deep@dagnu.de>
2024-03-31 22:12:57 +0100

    change working directory for flow file run

--- cmd/fbp/cli/run.go
+++ cmd/fbp/cli/run.go
@@ -2,30 +2,36 @@ package cli
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/dgf/go-fbp-x/dsl"
 	"github.com/dgf/go-fbp-x/network"
 )
 
-func Run(ctx context.Context, path string, trace bool) {
-	wg := sync.WaitGroup{}
-	out := make(chan string, 1)
-	traces := make(chan network.Trace, 1)
-
+func Run(ctx context.Context, path string, trace bool) error {
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatalf("Load failed: %v", err)
+		return fmt.Errorf("open failed: %w", err)
+	}
+
+	if err := os.Chdir(filepath.Dir(path)); err != nil {
+		return fmt.Errorf("chdir failed: %w", err)
 	}
 
 	graph, err := dsl.Parse(file)
 	if err != nil {
-		log.Fatalf("Parse failed: %v", err)
+		return fmt.Errorf("parse failed: %w", err)
 	}
 
+	var runErr error
+	out := make(chan string, 1)
+	traces := make(chan network.Trace, 1)
+
+	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -33,7 +39,7 @@ func Run(ctx context.Context, path string, trace bool) {
 		defer close(out)
 
 		if err := network.NewNetwork(NewFactory(out)).Run(ctx, graph, traces); err != nil {
-			log.Fatalf("Run failed: %v", err)
+			runErr = fmt.Errorf("run failed: %w", err)
 		}
 	}()
 
@@ -58,4 +64,5 @@ func Run(ctx context.Context, path string, trace bool) {
 	}()
 
 	wg.Wait()
+	return runErr
 }
