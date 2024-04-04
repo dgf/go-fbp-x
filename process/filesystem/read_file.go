1421d3782241a31d0cc63be105f421b75fed249c
dgf <deep@dagnu.de>
2024-04-04 20:04:07 +0100

    rename error channel

--- process/filesystem/read_file.go
+++ process/filesystem/read_file.go
@@ -10,29 +10,29 @@ import (
 )
 
 type readFile struct {
-	in   chan any
-	out  chan any
-	errs chan any
+	in  chan any
+	out chan any
+	err chan any
 }
 
 func ReadFile() process.Process {
 	rf := &readFile{
-		in:   make(chan any, 1),
-		out:  make(chan any, 1),
-		errs: make(chan any, 1),
+		in:  make(chan any, 1),
+		out: make(chan any, 1),
+		err: make(chan any, 1),
 	}
 
 	go func() {
-		defer close(rf.errs)
+		defer close(rf.err)
 		defer close(rf.out)
 
 		for i := range rf.in {
 			if s, ok := i.(string); !ok {
 				panic(fmt.Sprintf("Invalid fs/ReadFile input %q", i))
 			} else if file, err := os.Open(s); err != nil {
-				rf.errs <- err.Error()
+				rf.err <- err.Error()
 			} else if data, err := io.ReadAll(file); err != nil {
-				rf.errs <- err.Error()
+				rf.err <- err.Error()
 			} else {
 				rf.out <- strings.TrimRight(strings.TrimRight(string(data), "\n"), "\r")
 			}
@@ -55,6 +55,6 @@ func (rf *readFile) Inputs() map[string]process.Input {
 func (rf *readFile) Outputs() map[string]process.Output {
 	return map[string]process.Output{
 		"out": {Channel: rf.out, IPType: process.StringIP},
-		"err": {Channel: rf.errs, IPType: process.StringIP},
+		"err": {Channel: rf.err, IPType: process.StringIP},
 	}
 }
