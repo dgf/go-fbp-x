6af069083f682bece3d3939f442b1bab6e292eae
dgf <deep@dagnu.de>
2024-04-06 21:18:52 +0100

    add Go blog post extract Wasm example

--- web/fbp/wasm/factory.go
+++ web/fbp/wasm/factory.go
@@ -3,6 +3,8 @@ package wasm
 import (
 	"github.com/dgf/go-fbp-x/process"
 	"github.com/dgf/go-fbp-x/process/core"
+	"github.com/dgf/go-fbp-x/process/html"
+	"github.com/dgf/go-fbp-x/process/http"
 	"github.com/dgf/go-fbp-x/process/text"
 )
 
@@ -13,6 +15,8 @@ func NewFactory(out chan<- string) process.Factory {
 		"core/Kick":   func() process.Process { return core.Kick() },
 		"core/Output": func() process.Process { return core.Output(out) },
 		"core/Tick":   func() process.Process { return core.Tick() },
+		"html/Query":  func() process.Process { return html.Query() },
+		"http/Get":    func() process.Process { return http.Get() },
 		"text/Append": func() process.Process { return text.Append() },
 		"text/Split":  func() process.Process { return text.Split() },
 	})
